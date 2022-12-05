b943f36bb0a9af5f88322becb79e8f517bf43a70
Ganesh Thorat <thorat.ganeshscoe@gmail.com>
2022-12-05 15:25:23 +0530

    Moving from Heroku

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^xj2!s%q-v@d%towy0o071rlfwgsy=amfyr#u6s$8=fkk1r-b(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ganeshthorat.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*','ganeshthorat.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
